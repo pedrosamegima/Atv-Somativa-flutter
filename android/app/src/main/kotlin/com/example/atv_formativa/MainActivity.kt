cdb7e7dbc26635911773dff1d792e6abc7a2d7b4
pedrosamegima <phsamegima@gmail.com>
2024-04-04 13:34:54 -0300

    AtvSomativa

---
+++ android/app/src/main/kotlin/com/example/atv_formativa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atv_formativa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'imc.dart';
+import 'Contador.dart';
+import 'CadastroUser.dart';
+import 'CadastroProduto.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MENU"),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Icon(
+              Icons.add_business_sharp,
+              size: 120.0,
+              color: Colors.deepOrangeAccent,
+            ),
+            Text(
+              "Aplicação Principal",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+            ),
+            Text(
+              "",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+            ),
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  ElevatedButton(
+                      child: const Text("IMC"),
+                      onPressed: () {
+                      Navigator.pushNamed(context, '/imc');
+                      }),
+                  ElevatedButton(
+                      child: const Text("Contador"),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/contador');
+                      }),
+                  ElevatedButton(
+                      child: const Text("Cadastro Usuario"),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/CadastroUser');
+                      }),
+                  ElevatedButton(
+                      child: const Text("Cadastro de produtos"),
+                      onPressed: () {
+                       Navigator.pushNamed(context, '/CadastroProduto');
+                      }),
+                ]),
+          ],
+        ),
+      ), // Botão para executar o calculo do IMC,
+    );
+  }
+}
